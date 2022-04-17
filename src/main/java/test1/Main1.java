12cbf47f53c20d226cfb4ee0f85441a1e778c740
praveenrajbuggala <praveen8978470998rock@gmail.com>
2022-04-17 13:18:59 +0530

    initial commit

---
+++ src/main/java/test1/Main1.java
@@ -0,0 +1,22 @@
+package test1;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Main1 main1 = new Main1();
+        System.out.println(main1.returnString());
+        System.out.println(main1.returnString1());
+//        System.out.println(main1.returnString2());
+    }
+
+    public String returnString(){
+        return "hii";
+    }
+
+    public String returnString1(){
+        return "hello";
+    }
+
+    public String returnString2(){
+        return "hii how are u";
+    }
+}
