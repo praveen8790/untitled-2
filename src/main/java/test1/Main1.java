39bdede8b3385d582361bc4ea92aa7a01ce20130
praveenrajbuggala <praveen8978470998rock@gmail.com>
2022-04-17 13:32:53 +0530

    commented out code

--- src/main/java/test1/Main1.java
+++ src/main/java/test1/Main1.java
@@ -3,9 +3,9 @@ package test1;
 public class Main1 {
     public static void main(String[] args) {
         Main1 main1 = new Main1();
-        System.out.println(main1.returnString());
-        System.out.println(main1.returnString1());
-//        System.out.println(main1.returnString2());
+//        System.out.println(main1.returnString());
+//        System.out.println(main1.returnString1());
+        System.out.println(main1.returnString2());
     }
 
     public String returnString(){
